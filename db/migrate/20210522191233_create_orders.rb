3e17726846fe8cf3054bf9529e8f5259b4dbdc92
Gergo Karcagi <karcagigergo@gmail.com>
2021-05-22 16:14:14 -0300

    created basket and order models BOTH

---
+++ db/migrate/20210522191233_create_orders.rb
@@ -0,0 +1,11 @@
+class CreateOrders < ActiveRecord::Migration[6.0]
+  def change
+    create_table :orders do |t|
+      t.integer :quantity
+      t.references :product, null: false, foreign_key: true
+      t.references :basket, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
