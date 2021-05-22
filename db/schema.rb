eab70582159401d95a2109c731b560b4dec4e4de
Eduardo Scovino <eduardo.scovino@gmail.com>
2021-05-22 15:46:34 -0300

    Created signature model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_22_181552) do
+ActiveRecord::Schema.define(version: 2021_05_22_184248) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,15 @@ ActiveRecord::Schema.define(version: 2021_05_22_181552) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_products_on_user_id"
+  end
+
+  create_table "signatures", force: :cascade do |t|
+    t.string "title"
+    t.string "type_of"
+    t.float "price"
+    t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
