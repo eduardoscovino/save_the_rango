23eb7b301ea237f0c144c4194d29551a57b50eb7
Eduardo Scovino <eduardo.scovino@gmail.com>
2021-05-22 15:49:17 -0300

    added signature references to users

---
+++ db/migrate/20210522184727_add_signature_ref_to_users.rb
@@ -0,0 +1,5 @@
+class AddSignatureRefToUsers < ActiveRecord::Migration[6.0]
+  def change
+    add_reference :users, :signature, null: false, foreign_key: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_22_184248) do
+ActiveRecord::Schema.define(version: 2021_05_22_184727) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,9 +43,12 @@ ActiveRecord::Schema.define(version: 2021_05_22_184248) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "signature_id", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["signature_id"], name: "index_users_on_signature_id"
   end
 
   add_foreign_key "products", "users"
+  add_foreign_key "users", "signatures"
 end
