5692ad42f38cb8ef8dd6a9aad364c235bd2dfc74
Eduardo Scovino <eduardo.scovino@gmail.com>
2021-05-22 15:52:32 -0300

    added columns: first name, last name and role

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_22_184727) do
+ActiveRecord::Schema.define(version: 2021_05_22_185130) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,9 @@ ActiveRecord::Schema.define(version: 2021_05_22_184727) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "signature_id", null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.string "role"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["signature_id"], name: "index_users_on_signature_id"
