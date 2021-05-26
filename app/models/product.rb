be92cdd349bf53c2be2cc09925e1dac8f2ed4111
Gergo Karcagi <karcagigergo@gmail.com>
2021-05-26 00:21:16 -0300

    done, with the views too. Added pgsearch for products#index, it is working

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,10 @@
 class Product < ApplicationRecord
   has_many :orders
+
+  include PgSearch::Model
+  pg_search_scope :search_by_name,
+    against: [ :name ],
+    using: {
+      tsearch: { prefix: true }
+    }
 end
