16d2dd3e9f8f207741feb9fb5e03d7e881ca6ca1
eduardoscovino <eduardoscovino@gmail.com>
2021-05-27 20:58:44 -0300

    baskets controller created

---
+++ app/controllers/baskets_controller.rb
@@ -0,0 +1,30 @@
+class BasketsController < ApplicationController
+  before_action :set_baskets, only: [:show, :update, :destroy]
+  skip_before_action :verify_authenticity_token
+
+  def show
+    authorize @basket
+  end
+
+  def update
+    @basket.update(basket_params)
+
+    redirect_to basket_path(@basket)
+  end
+
+  def destroy
+    @basket.destroy
+
+    redirect_to products_path
+  end
+
+  private
+
+  def set_baskets
+    @basket = Basket.find(params[:id])
+  end
+
+  def basket_params
+    params.require(:basket).permit(:status)
+  end
+end
