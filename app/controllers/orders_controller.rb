a6cea7263a33039d02ddb6bbc06304503b46c2b2
Gergo Karcagi <karcagigergo@gmail.com>
2021-06-12 16:46:12 -0300

    added quantity to orders when in basket, also updted the navbar cart icon according to it

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,5 @@
 class OrdersController < ApplicationController
-  before_action :set_orders, only: [:destroy]  
+  before_action :set_orders, only: [:destroy, :add_unit, :reduce_unit]  
 
   def create
     if current_user.basket
@@ -11,19 +11,35 @@ class OrdersController < ApplicationController
     order = Order.new
     order.basket = basket
     order.product = Product.find(params[:product_id])
-    order.quantity = params[:quantity].to_i
+    order.quantity = 1
     authorize order
     if order.save
-      redirect_to basket_path(basket)
+      redirect_to products_path, notice: 'Product added to cart!'
     else
       render 'products/show'
     end
   end
 
+  def add_unit
+    # add unit to order
+    @order.quantity +=1
+    @order.save
+    authorize @order
+    redirect_to basket_path(current_user.basket)
+  end
+
+  def reduce_unit
+    # reduce unit to order
+    @order.quantity -= 1
+    @order.save
+    authorize @order
+    redirect_to basket_path(current_user.basket)
+  end
+
   def destroy
     @order.destroy
     authorize @order
-    redirect_to basket_path(current_user.basket)
+    redirect_to basket_path(current_user.basket), notice: 'Product removed from cart'
   end
 
   private
