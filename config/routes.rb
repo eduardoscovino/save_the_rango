36b60d2a3b245eb01773b9020e9dd5397c19e9bc
Gergo Karcagi <karcagigergo@gmail.com>
2022-01-17 15:43:48 -0300

    updated en.yml and the basket show and checkout views accordingly

--- config/routes.rb
+++ config/routes.rb
@@ -1,31 +1,27 @@
 Rails.application.routes.draw do
   devise_for :users
-  scope '(:locale)', locale: /pt/ do
+  scope '(:locale)', locale: /en|pt/ do
     root to: 'pages#home'
     resources :products, only: [:index, :show] do
       resources :orders, only: [:new, :create]
     end
-  end
-  resources :orders, only: [:update, :destroy] do 
-    member do
-      patch 'add_unit'
-      patch 'reduce_unit'
+    resources :orders, only: [:update, :destroy] do 
+      member do
+        patch 'add_unit'
+        patch 'reduce_unit'
+      end
     end
-  end
-  resources :baskets, only: [:show, :update, :destroy] do
-    member do
-      get 'checkout'
-      patch 'checkout'
+    resources :baskets, only: [:show, :update, :destroy] do
+      member do
+        get 'checkout'
+        patch 'checkout'
+      end
     end
+  
+    resources :signatures, only: [:index, :show] do
+      resources :subscriptions, only: [:create]
+    end
+  
+    resources :subscriptions, only: [:show, :checkout, :destroy]
   end
-
-  resources :signatures, only: [:index, :show] do
-    resources :subscriptions, only: [:create]
-  end
-
-  resources :subscriptions, only: [:show, :checkout, :destroy]
-
-  # post '/subscriptions/signature/:id', to: 'subscriptions#create', as: 'subscriptions'
-
-
 end
